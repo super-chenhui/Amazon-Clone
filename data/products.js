8d33b87cfa771d36be3edea6838f1f80dd660f28
Uxdruh <uxdruh@gmail.com>
2024-05-04 21:04:14 +0800

    Convert products into classes

--- data/products.js
+++ data/products.js
@@ -1,16 +1,33 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
 export function getProduct(productId) {
-  /*
-  let matchingProduct;
-  products.forEach(product => {
-    if (product.id === productId) {
-      matchingProduct = product;
-    }
-  });
-  */
-
-  // const matchingProduct = products.find(product => product.id === productId);
-
   return products.find(product => product.id === productId);
+}
+
+class Product {
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+  keywords;
+
+  constructor(productDetails) {
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+    this.keywords = productDetails.keywords;
+  }
+
+  getStarsUrl() {
+    return `images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+
+  getPrice() {
+    return `$${formatCurrency(this.priceCents)}`;
+  }
 }
 
 export const products = [
@@ -690,4 +707,6 @@ export const products = [
     },
     priceCents: 2999
   }
-];
+].map(productDetails => {
+  return new Product(productDetails);
+});
