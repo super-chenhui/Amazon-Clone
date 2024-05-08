33f54094d81daef6f3ea1c79070388c595adb469
Uxdruh <uxdruh@gmail.com>
2024-05-08 22:36:50 +0800

    Make search button interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -8,7 +8,28 @@ loadProducts(renderProductsGrid);
 function renderProductsGrid() {
   let productsHTML = '';
 
-  products.forEach(product => {
+  const url = new URL(window.location.href);
+  const search = url.searchParams.get('search');
+
+  let filteredProducts = products;
+
+  // If a search exists in the URL parameters,
+  // filter the products that match the search.
+  if (search) {
+    filteredProducts = products.filter(product => {
+      let matchingKeyword = false;
+
+      product.keywords.forEach(keyword => {
+        if (keyword.toLowerCase().includes(search.toLowerCase())) {
+          matchingKeyword = true;
+        }
+      });
+
+      return matchingKeyword || product.name.toLowerCase().includes(search.toLowerCase());
+    });
+  }
+
+  filteredProducts.forEach((product) => {
     productsHTML += `
       <div class="product-container">
         <div class="product-image-container">
@@ -108,4 +129,10 @@ function renderProductsGrid() {
 
     });
   })
+
+  document.querySelector('.js-search-button')
+    .addEventListener('click', () => {
+      const search = document.querySelector('.js-search-bar').value;
+      window.location.href = `amazon.html?search=${search}`;
+    });
 }
